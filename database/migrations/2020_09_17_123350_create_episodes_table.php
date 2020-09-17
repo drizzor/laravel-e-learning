3060ae285dae4d05a0f902a33967cb528a9a0cbf
Kevin <k.mary@live.be>
2020-09-17 15:01:01 +0200

    Init db and factory

---
+++ database/migrations/2020_09_17_123350_create_episodes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEpisodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('episodes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained()->cascadeOnDelete(); // Association d'ID. Utilisation de constrained() rend le code moins verbeux que la méthode classique
+            $table->string('title');
+            $table->text('description');
+            $table->string('video_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('episodes');
+    }
+}
